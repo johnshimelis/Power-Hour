77a773edfc7423d26a987a34c97ad500595d587d
John Shimelis <johnshimelis09@gmail.com>
2021-08-03 14:20:19 +0300

    Login done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+<template >
+  <div>
+
+<signin></signin>
+
+
+</div>
 </template>
+<script>
+ 
+import SignIn from './components/Signin.vue'
+
+  export default{
+    components :{
+      'signin' : SignIn
+    },
+    
+      
+    data(){
+      return{
+  
+      
+      } 
+    },
+    methods : {
+     
+    }
+    }
+</script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
